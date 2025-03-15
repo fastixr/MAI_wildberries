5d4135f79fb164416b3cbc0f80aa2ed22aa2cbae
Vladimir <143862659+fastixr@users.noreply.github.com>
2025-03-15 03:01:39 +0300

    merge obs

--- wildberries/src/components/Header.tsx
+++ wildberries/src/components/Header.tsx
@@ -85,8 +85,6 @@ const Header: React.FC = () => {
       document.body.classList.remove('overflow-hidden'); // Включаем прокрутку
     }
   };
-  
-  
 
   return (
     <header className="header">
@@ -96,7 +94,7 @@ const Header: React.FC = () => {
             <div className="flex-shrink-0 transform -translate-y-1">
               <Link href="/">
                 <Image
-                  src="/logo.svg"
+                  src="/logos/logo.svg"
                   alt="Логотип"
                   width={240}
                   height={38}
